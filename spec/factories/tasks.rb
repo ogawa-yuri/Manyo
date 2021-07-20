f481822a182be29b716ac268bf9131dacc4ed3b0
ogawa-yuri <nomiyuri713@gmail.com>
2021-07-20 20:45:55 +0900

    modelspec作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
-    content { 'test_content' }
+    title {'test_title'}
+    content {'test_content'}
   end
 
   factory :second_task, class: Task do
-    title {'second_task_title'}
-    content {'second_task_content'}
+    title {''}
+    content {'test_content2'}
+  end
+
+  factory :third_task, class: Task do
+    title {'test_title3'}
+    content {''}
   end
 end
