d16b39caddabf655c9892e3e789422478aebbe7d
ogawa-yuri <nomiyuri713@gmail.com>
2021-07-21 19:53:29 +0900

    国際化設定_view

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.time_zone = 'Tokyo'
 
 module Manyo
   class Application < Rails::Application
