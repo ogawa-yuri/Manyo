05e167b504bdd29737233ecbca96a2a2c9924adc
Yuri <nomiyuri713@gmail.com>
2021-08-09 07:54:26 +0900

    ユーザーモデル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( name: "yuri", email: "nomiyuri713@gmail.com", password: "q123456789@")
