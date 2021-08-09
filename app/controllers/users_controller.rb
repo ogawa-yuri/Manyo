f476a064e7373727f6e67604ad66504ae1989345
Yuri <nomiyuri713@gmail.com>
2021-08-09 20:37:05 +0900

    ログイン機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+    @user = User.new
+    redirect_to tasks_path if current_user.present?
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # redirect_to tasks_path
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+    redirect_to tasks_path unless @user == current_user
+  end
+  private
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation)
+  end
+end
